f9981df467f5d1e47e28bd065e7c10b6b1e2cb13
Chisom <83100384+titan-double07@users.noreply.github.com>
2025-08-30 13:06:16 +0100

    feat:Project setup/init - expo - nativewind - assets (images,icons, fonts)

---
+++ app/_layout.tsx
@@ -0,0 +1,42 @@
+import { useFonts } from 'expo-font';
+import 'react-native-reanimated';
+import "./global.css";
+
+import { SplashScreen, Stack } from 'expo-router';
+import { useEffect } from 'react';
+
+// Prevent auto-hiding the splash until fonts are ready
+SplashScreen.preventAutoHideAsync()
+export default function RootLayout() {
+
+  // this is used to load the fonts
+  const [fontsLoaded, fontError] = useFonts({
+    'Quicksand': require('../assets/fonts/Quicksand-Regular.ttf'),
+    'Quicksand-Bold': require('../assets/fonts/Quicksand-Bold.ttf'),
+    'Quicksand-SemiBold': require('../assets/fonts/Quicksand-SemiBold.ttf'),
+    'Quicksand-Light': require('../assets/fonts/Quicksand-Light.ttf'),
+    'Quicksand-Medium': require('../assets/fonts/Quicksand-Medium.ttf'),
+  });
+
+
+  // if there is an error loading the fonts, throw an error, if fonts are loaded, hide the splash screen
+  useEffect(() => {
+    if (fontError) {
+      console.log(fontError);
+      throw fontError;
+    }
+    if (fontsLoaded) {
+      console.log('fonts loaded');
+      SplashScreen.hideAsync(); // .hideAsync() will hide the splash screen after fonts are loaded
+    }
+  })
+
+  if (!fontsLoaded) {
+    return null // don't render UI until fonts are ready
+  }
+
+
+  return (
+    <Stack screenOptions={{ headerShown: false }} />
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+    return (
+        <View className="flex-1 items-center justify-center bg-white ">
+            <Text className="text-xl font-quicksand-bold  text-primary ">
+                Welcome to Nativewind!
+            </Text>
+        </View>
+    )
+}
